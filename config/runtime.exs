4bc8097d0a2b5f4351a2509af9f251361ce9a7d6
Muhammad Alfarizi Tazkia <alfatazkia@gmail.com>
2025-06-22 13:27:11 +0900

    'fix'

--- config/runtime.exs
+++ config/runtime.exs
@@ -1,7 +1,6 @@
 import Config
 
 if config_env() == :prod do
-  # Baca env vars penting
   secret_key_base =
     System.get_env("SECRET_KEY_BASE") ||
       raise """
@@ -9,15 +8,15 @@ if config_env() == :prod do
       You can generate one by calling: mix phx.gen.secret
       """
 
-  host = System.get_env("PHX_HOST") || "example.com"
+  ip_host = System.get_env("PHX_HOST") || "202.10.42.156"  # Ganti sesuai IP VPS kamu
   port = String.to_integer(System.get_env("PORT") || "4000")
 
   config :portfolio, PortfolioWeb.Endpoint,
-    url: [host: host, port: 443, scheme: "https"],
+    url: [host: ip_host, port: port],
     http: [ip: {0, 0, 0, 0}, port: port],
-    secret_key_base: secret_key_base
+    secret_key_base: secret_key_base,
+    check_origin: ["//#{ip_host}"]
 
-  # Database URL
   database_url =
     System.get_env("DATABASE_URL") ||
       raise "environment variable DATABASE_URL is missing."
@@ -25,5 +24,5 @@ if config_env() == :prod do
   config :portfolio, Portfolio.Repo,
     url: database_url,
     pool_size: String.to_integer(System.get_env("POOL_SIZE") || "10"),
-    ssl: true
+    ssl: false  # Jika PostgreSQL di localhost tanpa SSL, ubah jadi false
 end
